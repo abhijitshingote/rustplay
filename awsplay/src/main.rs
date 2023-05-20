31b8f0195f1ca51f720665639f16e938c71b9e27
abhijit shingote <abhijitshingote@gmail.com>
2023-05-20 12:28:54 -0400

    parse that json!

--- awsplay/src/main.rs
+++ awsplay/src/main.rs
@@ -1,27 +1,53 @@
 // cargo run -- s3://smrf-mrm-test/barry-test/prov_intel_test3/57/202302/rep_plan_group_id_534.parquet/npi=1003000399
 // s3://smrf-mrm-test/abi-test/prov_intel_mover/57/202302/npi=1003000399
 
+// cargo run -- s3://smrf-mrm/alliedbenefits/prepped/202305/2022-12-26-innrfppog12022022-cms_in_network_rates_ppo_2022-12-02_AK.json/in_network.jsonl 100
+
 use aws_sdk_s3 as s3;
 use aws_sdk_s3::Client;
+use clap::Parser;
 use tokio::io::{AsyncBufReadExt, BufReader};
+use std::{process};
+use serde::{Serialize,Deserialize};
+use serde_json;
+
 
 #[tokio::main]
 async fn main() -> Result<(), s3::Error> {
-    let args: Vec<String> = std::env::args().collect();
-    let source_path = parse_s3_uri(&args[1]);
-    let dest_path = parse_s3_uri(&args[2]);
-    println!(
-        "Source Path - {:?}\nDestination - {:?}",
-        source_path, dest_path
-    );
+    let args = Opt::parse();
     let config = aws_config::load_from_env().await;
     let client = s3::Client::new(&config);
-    copy(&client, source_path, dest_path).await;
-    let s3uri="s3://smrf-mrm-test/abi-test/aetnamrf-27440.csv/part-00000-b6babe28-9b16-4ab4-a36a-918926ae82fd-c000.csv";
-    let path = parse_s3_uri(s3uri);
+    // // copy(&client, source_path, dest_path).await;
+    let path = parse_s3_uri(&args.file_name);
     println!("{:?}", path);
-    read_s3_object(&path, &client, NumLines::Everything).await;
+    read_s3_object(&path, &client, NumLines::Num(args.num_lines)).await;
     Ok(())
+}
+#[derive(Default, Parser, Debug)]
+struct Opt {
+    file_name: String,
+    num_lines: i32,
+}
+
+#[derive(Serialize,Deserialize,Debug)]
+struct Rate {
+    billing_code: String,
+    billing_code_type: String,
+    negotiation_arrangement: String,
+    negotiated_rates: Vec<NegRate>
+}
+#[derive(Serialize,Deserialize,Debug)]
+struct NegPrice {
+    negotiated_type: String,
+    negotiated_rate: f32,
+    expiration_date: String,
+    service_code: Vec<String>,
+    billing_class: String
+}
+#[derive(Serialize,Deserialize,Debug)]
+struct NegRate {
+    negotiated_prices: Vec<NegPrice>,
+    provider_references: Vec<usize>
 }
 
 enum NumLines {
@@ -47,23 +73,19 @@ async fn read_s3_object(s3_path: &S3Path, client: &Client, n: NumLines) {
             let buf_reader = BufReader::new(obj.body.into_async_read());
             let mut lines = buf_reader.lines();
             // let l =lines.next_line()
-            for _ in 0..5 {
-                println!(
-                    "Printing data {:?}",
-                    lines
-                        .next_line()
-                        .await
-                        .expect("failed reading line")
-                        .unwrap()
-                );
-                println!(
-                    "Printing data {:?}",
-                    lines
-                        .next_line()
-                        .await
-                        .expect("failed reading line")
-                        .unwrap()
-                );
+            for _ in 0..num {
+                let line = lines
+                    .next_line()
+                    .await
+                    .unwrap_or_else(|err| {process::exit(0)})
+                    .unwrap_or_else(|| {
+                        println!("Maybe end of file!");
+                        process::exit(0);});
+                // let snip= line.find(r#""billing_code""#).unwrap();
+                // println!("{}", &line[snip..snip+22]);
+                // println!("{}",&line);
+                let r: Rate = serde_json::from_str(&line).unwrap();
+                println!("{:?},{:?}",r.billing_code,r.negotiated_rates[0].negotiated_prices[0].negotiated_rate);
             }
         }
     }
